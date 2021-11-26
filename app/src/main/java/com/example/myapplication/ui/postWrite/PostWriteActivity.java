30f803a131d368e18a2658e22257d4b1c8f61531
oscirt <a@example.com>
2021-11-26 23:27:03 +0300

    Add PostWrite Button on ActionBar(create new Action)

---
+++ app/src/main/java/com/example/myapplication/ui/postWrite/PostWriteActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.ui.postWrite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class PostWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_write);
+    }
+}
